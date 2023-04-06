7d1629322dce652c53054c29221cc6321dd80950
sskiragu <skiragu18@gmail.com>
2023-04-06 14:32:40 +0300

    JWT implementation

--- src/main/java/com/sampa/springapi/SecurityConfig.java
+++ src/main/java/com/sampa/springapi/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.sampa.springapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.sampa.springapi.filter.JwtAuthFilter;
 import com.sampa.springapi.repository.UserRepository;
 import com.sampa.springapi.service.UserInfoUserDetailsService;
 
@@ -25,6 +30,9 @@ public class SecurityConfig {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	 @Autowired
+	    private JwtAuthFilter authFilter;
 	
 	@Bean
 	public static PasswordEncoder passwordEncoder() {
@@ -36,28 +44,20 @@ public class SecurityConfig {
 	   
 	   http.csrf().disable()
 	   		.authorizeHttpRequests((authorize) -> authorize
-	   				.requestMatchers("/signup", "/roles/new", "/user-roles/new").permitAll()
+	   				.requestMatchers("/signup", "/roles/new", "/user-roles/new", "/login").permitAll()
 	   				.anyRequest().authenticated()
-	   				).httpBasic(Customizer.withDefaults());
+	   				).httpBasic(Customizer.withDefaults())
+	   		.sessionManagement()
+	   		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	   		.and()
+	   		.authenticationProvider(authenticationProvider())
+	   		.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
 	   		
 	   return http.build();
    }
    
    @Bean
    public UserDetailsService userDetailsService() {
-//	   UserDetails sam = User.builder()
-//			   .username("sam")
-//			   .password(passwordEncoder().encode("sam"))
-//			   .roles("USER")
-//			   .build();
-//	   
-//	   UserDetails admin = User.builder()
-//			   .username("admin")
-//			   .password(passwordEncoder().encode("admin"))
-//			   .roles("ADMIN")
-//			   .build();
-//	   
-//	   return new InMemoryUserDetailsManager(sam, admin);
 	   return new UserInfoUserDetailsService(userRepository);
    }
    
@@ -67,6 +67,11 @@ public class SecurityConfig {
 	   daoAuthenticationProvider.setUserDetailsService(userDetailsService());
 	   daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 	   return daoAuthenticationProvider; 
+   }
+   
+   @Bean
+   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+	   return config.getAuthenticationManager();
    }
 
 }
--- src/main/java/com/sampa/springapi/controller/AuthController.java
+++ src/main/java/com/sampa/springapi/controller/AuthController.java
@@ -3,12 +3,18 @@ package com.sampa.springapi.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sampa.springapi.dto.AuthRequest;
 import com.sampa.springapi.model.User;
 import com.sampa.springapi.service.AuthService;
+import com.sampa.springapi.service.JwtService;
 
 @RestController
 public class AuthController {
@@ -16,12 +22,22 @@ public class AuthController {
 	@Autowired
 	private AuthService authService;
 	
-//	@PostMapping("/login")
-//	public String login(@RequestBody Map<String, String> request) {
-//		String username = request.get("username");
-//		String password = request.get("password");
-//		return username + " " + password;
-//	}
+	@Autowired
+	private JwtService jwtService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@PostMapping("/login")
+	public String login(@RequestBody AuthRequest authRequest) {
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+		
+		if (authentication.isAuthenticated()) {
+			return jwtService.generateToken(authRequest.getUsername());
+		}else {
+			throw new UsernameNotFoundException("Invalid credeentials");
+		}
+	}
 	
 	@PostMapping("/signup")
 	public User signup(@RequestBody User user) {
